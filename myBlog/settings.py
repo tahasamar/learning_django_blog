d8769f1ee2c9e912a88d135ac456c0d66b7ecd56
samar <18118samar@gmail.com>
2025-07-10 20:16:40 +0330

    add static root

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
